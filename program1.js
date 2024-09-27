991f5875c889c8d9800f8f1911c3630f5600be06
tyrant0407 <amankushwaha1331@gmail.com>
2024-09-27 13:55:41 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -8,6 +8,8 @@ var isValid = function(s) {
     for(let char of s){
         if(char === '(' || char ==='{' || char ==='['){
             stack.push(char);
+        }else{
+            
         }
     }
 };
